a7de3c45c918ef69d895f44c6afeaec95b904340
Lee <charismafight@hotmail.com>
2019-06-09 23:45:50 +0800

    fix born of days grammar problem

--- TYE/admin.py
+++ TYE/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
 from .models import *
 
+
 # Register your models here.
+class ArchiveAdmin(admin.ModelAdmin):
+    def save_model(self, request, obj, form, change):
+        obj.get_days_of_born()
+        super().save_model(request, obj, form, change)
+
+
 admin.site.register([Archive])
--- TYE/models.py
+++ TYE/models.py
@@ -5,13 +5,15 @@ from datetime import datetime
 class Archive(models.Model):
     title = models.TextField('标题', max_length=100)
     description = models.TextField('描述', max_length=2000, null=True)
-    votes = models.IntegerField('投票数', default=0)
-    file = models.FileField('文件', default=None)
+    votes = models.IntegerField('投票数', default=0, editable=False)
+    file = models.FileField('文件', default=None, null=True)
+    img = models.ImageField('图片', null=True)
     pub_date = models.DateTimeField('发布时间', default=datetime.now())
-    days_of_born = datetime(year=2018, month=2, day=8)
+    days_of_born = models.IntegerField('出生天数', editable=False)
 
-    def days_of_born(self):
-        return self.pub_date.strftime('%Y')
+    def get_days_of_born(self):
+        self.days_of_born = (self.pub_date - datetime(2018, 2, 8)).days
+        return
 
     def __str__(self):
         return self.title
